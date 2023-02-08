3c5cfe297910c4cffe2702a3bba3009f2d828e39
Albert Chon <albert@injectivelabs.org>
2023-02-07 23:24:32 -0400

    feat: add a minimum validator commission of 2%
    only enforced upon creating or editing a validator

--- x/staking/types/commission.go
+++ x/staking/types/commission.go
@@ -66,6 +66,9 @@ func (cr CommissionRates) Validate() error {
 		// rate cannot be greater than the max rate
 		return ErrCommissionGTMaxRate
 
+	case cr.Rate.LT(MinCommissionRate):
+		// rate cannot be less than 2%
+		return ErrCommissionTooSmall
 	case cr.MaxChangeRate.IsNegative():
 		// change rate cannot be negative
 		return ErrCommissionChangeRateNegative
