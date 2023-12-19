3ef76dad3cf7d0033c84e6473adf003f668e402b
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2023-12-18 16:46:27 -0800

    cherry pick: 5c9627ded

--- baseapp/chain_stream.go
+++ baseapp/chain_stream.go
@@ -11,13 +11,11 @@ type StreamEvents struct {
 }
 
 func (app *BaseApp) AddStreamEvents(height int64, events []abci.Event, flush bool) {
-	go func() {
-		if app.EnableStreamer {
-			app.StreamEvents <- StreamEvents{
-				Events: events,
-				Height: uint64(height),
-				Flush:  flush,
-			}
+	if app.EnableStreamer {
+		app.StreamEvents <- StreamEvents{
+			Events: events,
+			Height: uint64(height),
+			Flush:  flush,
 		}
-	}()
+	}
 }
