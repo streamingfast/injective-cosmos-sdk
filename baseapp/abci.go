201e3a9b37516929140f25e183a165d7ffaaae02
Simon Noetzlin <simon.ntz@gmail.com>
2025-02-18 14:45:39 +0100

    * Bump SDK to use CometBFT to v1.0.1 and CometBFT api * Add CometBFT v1 buf registry commit to buf.yaml * Update tendermint to comet import in proto files * Replace tendermint to comet imports in Go files * Remove deprecated sr25519 support starting from CometBFT v1.0.1 * Update SDK go.mod to use local ./api, which is upgraded to Comet v1, instead of cosmossdk.io/api v0.7.5 * Generate proto and pulsar files and remove tendermint from ./proto

--- baseapp/abci.go
+++ baseapp/abci.go
@@ -11,7 +11,7 @@ import (
 	"github.com/InjectiveLabs/metrics"
 	"github.com/cockroachdb/errors"
 	abci "github.com/cometbft/cometbft/abci/types"
-	cmtproto "github.com/cometbft/cometbft/proto/tendermint/types"
+	cmtproto "github.com/cometbft/cometbft/api/cometbft/types/v1"
 	"github.com/cosmos/gogoproto/proto"
 	"google.golang.org/grpc/codes"
 	grpcstatus "google.golang.org/grpc/status"
